7be243b025f44675c0404fcfcbf70a90b8b689af
TRIJAL28 <trijalsrivastav@gmail.com>
2026-04-10 11:53:16 +0530

    Remove .env from git tracking

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors({
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'https://bloodbridge-frontend.vercel.app',
+    'https://bloodbridge-frontend-git-main.vercel.app',
   ],
   credentials: true,
 }));
